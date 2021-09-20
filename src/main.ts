4bc9dbe9665fffc5544510eb1526bfba48c78470
carla-reis-cr <carla.cr.reis@gmail.com>
2021-09-20 17:45:20 -0300

    configuração da porta do main

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT || 3000;
   app.enableCors({origin: true, credentials: true});
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}.`);
+  });
 }
 bootstrap();
